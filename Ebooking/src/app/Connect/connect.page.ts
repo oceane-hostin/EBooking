791c296c34718ccb1d388a3a8951bbf7df9c22c9
Diane <diane.djunga@gmail.com>
2020-02-23 21:59:42 +0100

    modif account et home page + création du dossier connect

---
+++ Ebooking/src/app/Connect/connect.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-connect',
+    templateUrl: 'connect.page.html',
+    styleUrls: ['connect.page.scss']
+})
+export class ConnectPage {
+
+    constructor() { }
+
+}
