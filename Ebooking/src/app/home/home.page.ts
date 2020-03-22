a5d9131f5be32de4027dd2ef1d45b321db685fc2
Oceane <oceane.hostin@gmail.com>
2020-03-22 16:02:34 +0100

    Home Page

--- Ebooking/src/app/home/home.page.ts
+++ Ebooking/src/app/home/home.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
+
 export class HomePage {
+  public cities;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    var apiBaseUrl = "http://127.0.0.1:8000/"; // TODO: put the right base url somewhere
+    this.http.get(apiBaseUrl + 'housing/cities').subscribe((response) => {
+      if(response != null) {
+        this.cities = response;
+      } else {
+        this.cities = "notfound";
+      }
+    });
 
+  }
 }
