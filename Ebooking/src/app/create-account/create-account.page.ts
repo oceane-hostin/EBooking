abe8280851fee1ca73cea5aa4d3c0f2585f56645
Oceane <oceane.hostin@gmail.com>
2020-03-28 15:55:46 +0100

    Debut inscription mais pb de format de date avec l'api

--- Ebooking/src/app/create-account/create-account.page.ts
+++ Ebooking/src/app/create-account/create-account.page.ts
@@ -1,16 +1,57 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-create-account',
-  templateUrl: 'create-account.page.html',
-  styleUrls: ['create-account.page.scss']
+    selector: 'app-create-account',
+    templateUrl: 'create-account.page.html',
+    styleUrls: ['create-account.page.scss']
 })
 export class CreateAccountPage {
 
-  constructor(private storage: Storage) {
+    constructor(private http: HttpClient, private storage: Storage) {
 
-  }
+    }
+    /*{
+        "first_name": "Jane",
+        "last_name": "Doe",
+        "email": "jane.doe@gmail.com",
+        "password": "123",
+        "date_of_birth": "1990-02-02T00:00:00+01:00",
+        "is_admin": false
+    }*/
 
+    register() {
+        // @ts-ignore
+        var firstName = this.fist_name;
+        // @ts-ignore
+        var lastName = this.last_name;
+        // @ts-ignore
+        var email = this.email;
+        // @ts-ignore
+        var password = this.password;
+        // @ts-ignore
+        var dateOfBirth = this.date_of_birth;
+
+        var person = '{ "first_name": "'+firstName+'", "last_name": "'+lastName+'", "email": "'+email+'", "password": "'+password+'", "date_of_birth": "'+dateOfBirth+'", "is_admin": false }';
+
+        console.log(person);
+
+        var apiBaseUrl = "http://127.0.0.1:8000/";
+        this.http.post(apiBaseUrl + 'person/create', person).subscribe((response) => {
+            // response having status and info values
+            // @ts-ignore
+            if(response.status == "success") {
+                // @ts-ignore
+                this.storage.set("session", response.info);
+                window.location.href = "/tabs/account/";
+            } else {
+                // @ts-ignore
+                console.log(response.info);
+                //window.location.reload()
+            }
+        });
+
+    }
 
 }
--- Ebooking/src/app/login/login.page.ts
+++ Ebooking/src/app/login/login.page.ts
@@ -14,14 +14,18 @@ export class LoginPage {
   }
 
   login() {
+    // @ts-ignore
     var email = this.email;
+    // @ts-ignore
     var password = this.password;
 
     var apiBaseUrl = "http://127.0.0.1:8000/";
 
     this.http.post(apiBaseUrl + 'person/connect', {'email' :email, 'password' : password}).subscribe((response) => {
       // response having status and info values
+      // @ts-ignore
       if(response.status == "success") {
+        // @ts-ignore
         this.storage.set("session", response.info);
         window.location.href = "/tabs/account/";
       } else {
