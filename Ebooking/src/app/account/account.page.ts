2f2c17ecd7b09303d9b8fbf78ab1b339b1aab3a3
Oceane <oceane.hostin@gmail.com>
2020-03-29 18:30:09 +0200

    fin inscription + debut profil

--- Ebooking/src/app/account/account.page.ts
+++ Ebooking/src/app/account/account.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-account',
@@ -7,13 +8,58 @@ import { Storage } from '@ionic/storage';
   styleUrls: ['account.page.scss']
 })
 export class AccountPage {
+  private id;
+  private firstName;
+  private lastName;
+  private email;
+  private dateOfBirth;
+  private housings;
+  private bookings;
 
-  constructor(private storage: Storage) {
+
+  constructor(private storage: Storage, private http: HttpClient) {
     storage.get('session').then((val) => {
       if(val == null) {
         window.location.href = "/tabs/account/login";
+      } else {
+        this.id = val;
+        var apiBaseUrl = "http://127.0.0.1:8000/";
+        this.http.get(apiBaseUrl + 'person/read/id/'+ this.id).subscribe((response) => {
+          // @ts-ignore
+          this.firstName = response.first_name;
+          // @ts-ignore
+          this.lastName = response.last_name;
+          // @ts-ignore
+          this.email = response.email;
+          // @ts-ignore
+          var date = response.date_of_birth;
+          date = date.split('CEST')[0];
+          this.dateOfBirth = new Date(date);
+          // @ts-ignore
+          console.log(response.housings);
+          // @ts-ignore
+          this.housings = response.housings;
+          // @ts-ignore
+          this.bookings = response.bookings;
+        });
       }
     });
+
+
+/*{
+    "id": 8,
+    "first_name": "undefined",
+    "last_name": "a",
+    "email": "a",
+    "password": "123",
+    "date_of_birth": "1999-01-01T15:31:16.000000CEST",
+    "is_admin": false,
+    "created_at": "2020-03-29T15:32:47.000000CEST",
+    "updated_at": "2020-03-29T15:32:47.000000CEST",
+    "housings": [],
+    "bookings": []
+}*/
+
   }
 
   logout() {
--- Ebooking/src/app/create-account/create-account.page.ts
+++ Ebooking/src/app/create-account/create-account.page.ts
@@ -12,18 +12,10 @@ export class CreateAccountPage {
     constructor(private http: HttpClient, private storage: Storage) {
 
     }
-    /*{
-        "first_name": "Jane",
-        "last_name": "Doe",
-        "email": "jane.doe@gmail.com",
-        "password": "123",
-        "date_of_birth": "1990-02-02T00:00:00+01:00",
-        "is_admin": false
-    }*/
 
     register() {
         // @ts-ignore
-        var firstName = this.fist_name;
+        var firstName = this.first_name;
         // @ts-ignore
         var lastName = this.last_name;
         // @ts-ignore
@@ -35,20 +27,17 @@ export class CreateAccountPage {
 
         var person = '{ "first_name": "'+firstName+'", "last_name": "'+lastName+'", "email": "'+email+'", "password": "'+password+'", "date_of_birth": "'+dateOfBirth+'", "is_admin": false }';
 
-        console.log(person);
-
         var apiBaseUrl = "http://127.0.0.1:8000/";
         this.http.post(apiBaseUrl + 'person/create', person).subscribe((response) => {
             // response having status and info values
             // @ts-ignore
             if(response.status == "success") {
+                console.log(response);
                 // @ts-ignore
                 this.storage.set("session", response.info);
                 window.location.href = "/tabs/account/";
             } else {
-                // @ts-ignore
-                console.log(response.info);
-                //window.location.reload()
+                window.location.reload()
             }
         });
 
